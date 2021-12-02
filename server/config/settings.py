08e8fe3b426888abb9ea6e5184a9c708db708e00
Vadim Alperovich <39128279+VirtualRoyalty@users.noreply.github.com>
2021-12-02 19:27:57 +0300

    Update settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -16,20 +16,3 @@ class DevConfig(BaseConfig):
     POSTGRES_USER = os.environ.get('POSTGRES_USER', 'admin')
     POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'admin')
     SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
-    # CELERY_BROKER = 'pyamqp://rabbit_user:rabbit_password@broker-rabbitmq//'
-    # CELERY_RESULT_BACKEND = 'rpc://rabbit_user:rabbit_password@broker-rabbitmq//'
-
-
-# class ProductionConfig(BaseConfig):
-#     FLASK_ENV = 'production'
-#     SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_password@db-postgres:5432/flask-deploy'
-#     CELERY_BROKER = 'pyamqp://rabbit_user:rabbit_password@broker-rabbitmq//'
-#     CELERY_RESULT_BACKEND = 'rpc://rabbit_user:rabbit_password@broker-rabbitmq//'
-#
-#
-# class TestConfig(BaseConfig):
-#     FLASK_ENV = 'development'
-#     TESTING = True
-#     DEBUG = True
-#     # make celery execute tasks synchronously in the same process
-#     CELERY_ALWAYS_EAGER = True
